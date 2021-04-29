ef1ae3c37a361933d64754f69b27598ae43cfd8f
v.trubicin <v.trubicin@unitcode.ru>
2021-04-29 16:55:31 +0300

    Закоммитил

--- src/app/parallax/planets-mouse-move/planets-mouse-move.component.ts
+++ src/app/parallax/planets-mouse-move/planets-mouse-move.component.ts
@@ -1,4 +1,5 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import Swiper from 'swiper';
 
 declare const Parallax: any;
 
@@ -10,11 +11,34 @@ declare const Parallax: any;
 export class PlanetsMouseMoveComponent implements OnInit {
   @ViewChild('scene', {static: true}) sceneRef: ElementRef;
 
+  keys = [
+    'Mercury',
+    'Venus',
+    'Earth',
+    'Earth',
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
     const scene = this.sceneRef.nativeElement;
     const parallax = new Parallax(scene);
+
+    const slider = new Swiper('.swiper-container', {
+      slidesPerView: 'auto',
+      spaceBetween: 150,
+      centeredSlides: true,
+      mousewheel: true,
+      pagination: {
+        el: '.planet-links',
+        clickable: true,
+        renderBullet: function(index, className) {
+          return '<div class="' + className + '">' + this.keys[index + 1] + '</div>';
+        }
+      },
+    });
+
+
   }
 
 }
